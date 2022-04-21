40125aa67eeb89c9fb62bc063b43285c24a68b42
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-21 22:45:42 +0430

    added dead flag

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -7,18 +7,23 @@ struct Options {
     #[structopt(default_value = "Meow!")]
     /// What does the cat say?
     message: String,
+
+    #[structopt(short = "d", long = "dead")]
+    /// Make the cat appera dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::from_args();
     let message = options.message;
+    let eye = if options.dead { "x" } else { "o" };
 
     println!("{}", message);
     println!(" \\");
     println!("  \\");
-    println!("      /\\_/\\");
-    println!("     ( o  o )");
-    println!("      =( )=");
+    println!("       /\\_/\\");
+    println!("     ( {eye}   {eye} )", eye = eye);
+    println!("      =( I )=");
 }
 
 // cargo run -- "Hello world!"
