3ee71b9bcea741f3d745b2520cea4a39a2f5c27c
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-25 20:18:30 +0430

    listen to Esc key to exit

--- catsay_TUI/src/main.rs
+++ catsay_TUI/src/main.rs
@@ -1,17 +1,22 @@
 extern crate cursive;
 
+use cursive::event::Key;
 use cursive::views::TextView;
 
 fn main() {
-    let mut siv = cursive::default();
+  let mut siv = cursive::default();
 
-    let cat_text = "Meow!
+  let cat_text = "Meow!
   \\
    \\
     /\\__/\\
     ( o o )
     =( I )=";
 
-    siv.add_layer(TextView::new(cat_text));
-    siv.run();
+  siv.add_layer(TextView::new(cat_text));
+
+  // Listen to Key::Esc and quit
+  siv.add_global_callback(Key::Esc, |s| s.quit());
+
+  siv.run();
 }
