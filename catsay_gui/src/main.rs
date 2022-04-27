aa3b65b985893d5c6485b453a7a6f5dfe64b2d87
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-27 21:22:26 +0430

    added a simple gui with gtk

---
+++ catsay_gui/src/main.rs
@@ -0,0 +1,32 @@
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use gtk::{Application, ApplicationWindow, Button};
+
+fn main() {
+    let application = Application::builder()
+        .application_id("com.shayan.catsay")
+        .build();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("First GTK App")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click Me!");
+
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+    application.run();
+}
